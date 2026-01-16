d101f00bf896a0778b805ecd0f904f1319798da4
Rajendra More <158751784+rajendra0303@users.noreply.github.com>
2026-01-16 16:44:57 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Employee_Attendance_2026.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
